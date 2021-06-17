9b2475c673e68514ce5d9ed5919138560b4daeb4
Mo-Official <inf19151@lehre.dhbw-stuttgart.de>
2021-06-17 02:10:07 +0200

    added movement, wall colision and example for tail

---
+++ src/components/player.py
@@ -0,0 +1,100 @@
+from src import settings
+from typing import List, Tuple
+
+import pygame as pg
+import pygame.sprite as pgsprite
+import pygame.draw as pgdraw
+
+import pygame.math as pgmath
+vec2 = pgmath.Vector2
+
+
+class Player(pgsprite.Sprite):
+    """Class for the player's snake
+    
+    Attributes
+    ----------
+    - length: The amount of nodes the snake has
+    - dir: the way the snake is facing
+    - pos: the square, where the head of the snake is spawned
+    """
+    dir = "right"
+    points = 0
+    length = 1
+    last_move = 0
+
+    speed = 1.
+
+
+    def __init__(self, pos) -> None:
+        """
+        Param
+        -----
+            - groups: optional parameter for the sprite's groups
+
+                Default: None
+        """
+        super().__init__()
+        self.image = pg.Surface((64,64))
+        self.image.fill(pg.Color("blue"))
+        self.rect = self.image.get_rect()
+
+        # a simple tail
+        self.tail_image = pg.Surface((64,64))
+        self.tail_image.fill(pg.Color("green"))
+        self.tail_rect = self.tail_image.get_rect()
+        self.tail_pos = vec2(*pos)
+        self.tail_rect.topleft = self.tail_pos
+        
+        self.pos = pos
+        self.rect.topleft = self.pos
+        self.movement_vec = vec2(0,0)
+
+    def get_key_up(self, event):
+        if event.key == pg.K_UP:
+            self.dir = "up"
+        elif event.key == pg.K_DOWN:
+            self.dir = "down"
+        elif event.key == pg.K_LEFT:
+            self.dir = "left"
+        elif event.key == pg.K_RIGHT:
+            self.dir = "right"
+
+    def move(self):
+        last_move_vec = self.movement_vec # last movement passed to the pos of the next snake part
+
+        if self.dir == "left":
+            self.movement_vec = (-64, 0)
+        if self.dir == "right":
+            self.movement_vec = (64, 0)
+        if self.dir == "up":
+            self.movement_vec = (0, -64)
+        if self.dir == "down":
+            self.movement_vec = (0, 64)
+        
+        self.pos += self.movement_vec
+        self.tail_pos += last_move_vec
+
+        self.rect.topleft = self.pos
+        self.tail_rect.topleft = self.tail_pos
+
+
+    def update(self):
+        self.last_move += self.speed
+        if self.last_move > 60:
+            self.move()
+            self.last_move = 0
+            
+            
+
+    def eat(self, food):
+        if self.length < 10:
+            self.length += 1
+            self.speed += 0.1
+        else:
+            self.speed += 0.2
+        food.kill()
+
+    def draw(self, surface):
+        surface.blit(self.image, self.rect)
+        surface.blit(self.tail_image, self.tail_rect)
--- src/states/gameplay.py
+++ src/states/gameplay.py
@@ -1,30 +1,71 @@
+from src import settings
+from src.components.player import Player
+from src.components.item_sprites import BaseItem
+
 from .game_over import GameOver
 from ..game import Game
 import pygame as pg
+
+import pygame.sprite as pgsprite
+import pygame.draw as pgdraw
+
+import pygame.math as pgmath
+vec2 = pgmath.Vector2
 
 from .base import BaseState
 
 class Gameplay(BaseState):
     def __init__(self) -> None:
         super(Gameplay, self).__init__()
-        self.rect = pg.Rect((0,0), (50,50))
         self.next_state = "GAME_OVER"
+
+    def startup(self, persistent):
+        self.all_sprites = pgsprite.Group()
+        self.all_food = pgsprite.Group()
+
+        self.snake = Player(vec2(64,0))
+        self.foods = [BaseItem((640,640), 100, 1000),
+                        BaseItem((640,0), 100, 1000),
+                        BaseItem((0,640),100, 1000)]
+
+        self.last_update = pg.time.get_ticks()
+
+        self.all_sprites.add(self.snake)
+        self.all_sprites.add(self.foods)
+
+        self.all_food.add(self.foods)
+        print("WAHTTAS")
+
 
     def get_event(self, event):
         if event.type == pg.QUIT:
             self.quit = True
         elif event.type == pg.KEYUP:
-            if event.key == pg.K_UP:
-                self.rect.move_ip(0, -10)
-            elif event.key == pg.K_DOWN:
-                self.rect.move_ip(0, 10)
-            elif event.key == pg.K_LEFT:
-                self.rect.move_ip(-10, 0)
-            elif event.key == pg.K_RIGHT:
-                self.rect.move_ip(10, 0)
-            elif event.key == pg.K_SPACE:
-                self.done = True
+
+            self.snake.get_key_up(event=event)
+
+
+    def update(self, dt):
+        print(self.snake.rect.topleft)
+        # move the sanke
+        self.snake.update()
+
+        food_hits = pgsprite.spritecollide(self.snake, self.all_food, True)
+        for hit in food_hits:
+            print("Ate something")
+
+        # check if snake collided with a wall
+        snake_rect = self.snake.rect
+        if snake_rect.left < 0 or\
+               snake_rect.right > settings.WINDOW_WIDTH or\
+               snake_rect.top < 0 or\
+               snake_rect.bottom > settings.WINDOW_WIDTH:
+            self.done = True
+
+
 
     def draw(self, surface):
         surface.fill(pg.Color("black"))
-        pg.draw.rect(surface, pg.Color("blue"), self.rect)
+        self.all_sprites.draw(surface)
+        self.snake.draw(surface)
+
