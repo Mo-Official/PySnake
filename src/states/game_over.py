a001f54f0bda8412c8195a0bb5d516413393be6f
Mo-Official <inf19151@lehre.dhbw-stuttgart.de>
2021-06-27 23:12:05 +0200

    added death_by text to the gameover screen

--- src/states/game_over.py
+++ src/states/game_over.py
@@ -1,5 +1,4 @@
 import pygame as pg
-
 from .base import BaseState
 
 class GameOver(BaseState):
@@ -7,13 +6,23 @@ class GameOver(BaseState):
         super(GameOver, self).__init__()
         self.title = self.font.render("Game Over", True, pg.Color("white"))
         self.title_rect = self.title.get_rect(center=self.screen_rect.center)
+        
+
+        
+
+    def startup(self, persistent):
+        self.texts = []
+
         self.instuctions = self.font.render("Press space to start again, or enter to got to the menu", True, pg.Color("white"))
         instructions_center = (self.screen_rect.center[0], self.screen_rect.center[1] + 50)
         self.instuctions_text = self.instuctions.get_rect(center = instructions_center)
+        self.texts.append((self.instuctions, self.instuctions_text))
 
-    def startup(self, persistent):
+        self.death_by = self.font.render(persistent["death_by"], True, pg.Color("white"))
+        death_by_center = (self.screen_rect.center[0], self.screen_rect.center[1] - 100)
+        self.death_by_text = self.death_by.get_rect(center = death_by_center)
+        self.texts.append((self.death_by, self.death_by_text))
         
-        return super().startup(persistent)
 
     def get_event(self, event):
         if event.type == pg.QUIT:
@@ -33,4 +42,5 @@ class GameOver(BaseState):
     def draw(self, surface):
         surface.fill(pg.Color("black"))
         surface.blit(self.title, self.title_rect)
-        surface.blit(self.instuctions, self.instuctions_text)
+        for text in self.texts:
+            surface.blit(*text)
