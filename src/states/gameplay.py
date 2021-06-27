92d9c6be20e837c6b86bb134958fea7e9bb1ee49
Mo-Official <inf19151@lehre.dhbw-stuttgart.de>
2021-06-27 23:13:35 +0200

    added checks to prevent overlapping when spawning food. Also saved images inside image_data dict

--- src/states/gameplay.py
+++ src/states/gameplay.py
@@ -1,3 +1,4 @@
+from src.img_manager import Img
 from src import settings
 from src.components.player import Player
 from src.components.item_sprites import BaseItem
@@ -20,8 +21,15 @@ class Gameplay(BaseState):
     def __init__(self) -> None:
         super(Gameplay, self).__init__()
         self.next_state = "GAME_OVER"
+        self.image_data = {}
 
     def startup(self, persistent):
+
+        if "img_data" in persistent:
+            self.image_data = persistent["img_data"]
+        else:
+            self.load_images()
+
         self.all_sprites = pgsprite.Group()
         self.all_food = pgsprite.Group()
 
@@ -35,6 +43,11 @@ class Gameplay(BaseState):
 
         self.all_food.add(self.foods)
 
+    def load_images(self):
+        self.image_data["food_orange"] = Img("data/img/food_orange.png", pg.Color("white"))
+        self.image_data["food_banana"] = Img("data/img/food_banana.png", pg.Color("white"))
+        
+
 
     def get_event(self, event):
         if event.type == pg.QUIT:
@@ -47,7 +60,7 @@ class Gameplay(BaseState):
     def update(self, dt):
 
         if self.snake.rect.collidelistall(self.snake.tail_rects):
-            self.persist["death_by"] = "You ate yourself!!"
+            self.persist["death_by"] = "You bit yourself!!"
             self.done = True
 
         # move the sanke
@@ -60,18 +73,49 @@ class Gameplay(BaseState):
             self.snake.length += 1
 
         if len(self.all_food) == 0:
-            print("OKAY")
             for _ in range(random.randint(3, 6)):
-                x = max(0, 64*random.randint(0,10))
-                x = min(x, settings.WINDOW_WIDTH)
 
-                y = max(0, 64*random.randint(0,10))
-                y = min(y, settings.WINDOW_HEIGHT)
+                allowed = False
+                while not allowed:
+                    x = max(0, 64*random.randint(0,10))
+                    x = min(x, settings.WINDOW_WIDTH)
 
-                food = BaseItem((x, y),100,1000)
+                    y = max(0, 64*random.randint(0,10))
+                    y = min(y, settings.WINDOW_HEIGHT)
+
+                    # check if food overlaps with snake
+                    snake_parts_check = True
+                    for part_rect in self.snake.tail_rects:
+                        if part_rect.x == x and part_rect.y == y:
+                            snake_parts_check = False
+                            break
+                    if not snake_parts_check:
+                        continue
+
+                    # check if food overlaps with other food
+                    food_overlap_check = True
+                    for food in self.all_food:
+                        if food.rect.x == x and food.rect.y == y:
+                            food_overlap_check = False
+                            break
+                    if not food_overlap_check:
+                        continue
+
+                    # if all checks succeed, then its allowed
+                    if snake_parts_check\
+                            and food_overlap_check:
+                        allowed = True
+                    
+
+                food_image = random.choice(
+                    [self.image_data["food_orange"].image,
+                    self.image_data["food_banana"].image
+
+                    ])
+                
+                food = BaseItem((x, y),100,1000, food_image)
                 self.all_food.add(food)
                 self.all_sprites.add(food)
-                print("YAY")
 
         
 
@@ -82,7 +126,7 @@ class Gameplay(BaseState):
         if snake_rect.left < 0 or\
                snake_rect.right > settings.WINDOW_WIDTH or\
                snake_rect.top < 0 or\
-               snake_rect.bottom > settings.WINDOW_WIDTH:
+               snake_rect.bottom > settings.WINDOW_HEIGHT:
             self.persist["death_by"] = "You hit a wall!!"
             self.done = True
 
